cf7e1020ed5f6bfe6adeb23225c35c3a4cf18fe0
PawelPD <pawlo1508@o2.pl>
2020-01-08 20:55:32 +0100

    first commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -19,14 +19,11 @@ public class FirstTest {
     @Test(priority = 2)
     void test()
     {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "1\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://pdsklep.e-kei.pl/");
-        driver.findElement(By.xpath("/html/body/main/header/div[2]/div/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
-        driver.findElement(By.xpath("//*[@id=\"category\"]")).click();
-        driver.findElement(By.xpath("/html/body/main/section/div/div[2]/section/section/div[1]/div/div[2]/div/div[1]/button")).click();
-        driver.get("https://pdsklep.e-kei.pl/6-akcesoria?order=product.price.asc");
+        System.out.println("Przeglądarka otworzona");
         driver.close();
     }
 
