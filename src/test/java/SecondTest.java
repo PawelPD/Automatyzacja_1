d68550c866d2de5c4ba19d0255c9f3f437901836
PawelPD <pawlo1508@o2.pl>
2020-01-09 22:44:00 +0100

    second commit

--- src/test/java/SecondTest.java
+++ src/test/java/SecondTest.java
@@ -1,10 +1,19 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.TestException;
 import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
 
 
 public class SecondTest {
@@ -18,31 +27,28 @@ public class SecondTest {
     }
 
     @Test(priority = 2)
-    void test()
-    {
+    void test() throws InterruptedException, IOException {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
-        WebDriver driver = new ChromeDriver(options);
+        //ChromeOptions options = new ChromeOptions();
+       // options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
+        WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://pdsklep.e-kei.pl/");
         System.out.println("Przeglądarka otworzona Second class");
 		driver.findElement(By.xpath("/html/body/main/header/div[2]/div/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
-		driver.findElement(By.xpath("/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[1]/div/a/img")).click();
+        driver.findElement(By.xpath("/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[1]/div/div[1]/h2/a")).click();
+        driver.findElement(By.xpath("/html/body/main/section/div/div/section/div[1]/div[2]/div[2]/div[2]/form/div[2]/div/div[2]/button")).click();
+        //driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div[2]/div/div/a")).click();
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("screenshot.png"));
+        sleep(5000);
         driver.close();
     }
 
-	@Test(priority = 3)
+    @Test(priority = 3)
     void teardown()
     {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		System.out.println("jakas strona second class");
-        driver.get("https://df");
-		System.out.println("po second class");
+        System.out.println("close second class");
     }
 
 
