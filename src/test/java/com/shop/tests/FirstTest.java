63a8fd80983d203cda3413015c4db9468088b023
PawelPD <59619921+PawelPD@users.noreply.github.com>
2020-01-19 22:29:25 +0100

    spowodowanie błędu budowy
    usunięcie klamry klasy

--- src/test/java/com/shop/tests/FirstTest.java
+++ src/test/java/com/shop/tests/FirstTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class FirstTest extends TestBase {
+public class FirstTest extends TestBase 
 
     HomePage homePage;
     LoginPage loginPage;
